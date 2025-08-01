21d0b28e5c5b70fec312e9c07bdd8a7e416d3818
Patricia Morse <patricia.d.morse@gmail.com>
2025-08-01 16:34:58 -0700

    Update index and JS for GitHub Pages site

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Automation", "Tax Regulation", "Organization", "BusinessAnalyst", "Team building", "Project Managment"],
+    strings: ["Automation", "Tax Regulation", "Organization", "Data Governance", "Business Analytics", "Team building", "Project Managment"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
